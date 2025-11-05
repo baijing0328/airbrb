6eebd57677e00dab4a7c2b23c6549e75085d26ee
Alex Wang <1262171417@qq.com>
2025-11-06 02:20:26 +1100

    feat: add create host list modal

--- frontend/src/pages/Listing/Host.jsx
+++ frontend/src/pages/Listing/Host.jsx
@@ -1,5 +1,6 @@
-import { Layout, Typography, Button,Flex } from "antd";
-import { UnorderedListOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { Layout, Typography, Button, Flex, Form, Input, Modal } from "antd";
+import { UnorderedListOutlined, PlusOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import LogoutBtn from "../../components/LogoutBtn";
 import HostItem from "../../components/HostItem";
@@ -13,14 +14,21 @@ const renderHostItems = (listings) => {
   return listings.map((listing) => (
     <HostItem key={listing.id} listing={listing} />
   ));
-}
-
+};
 
 const Host = () => {
   const navigate = useNavigate();
-
+  const [open, setOpen] = useState(false);
+  const [form] = Form.useForm();
+  const [formValues, setFormValues] = useState();
   const handleToggle = () => {
     navigate("/all");
+  };
+
+  const onCreate = (values) => {
+    console.log("Received values of form: ", values);
+    setFormValues(values);
+    setOpen(false);
   };
 
   return (
@@ -39,16 +47,60 @@ const Host = () => {
           >
             All Listings
           </Button>
+          <Button
+            type="primary"
+            onClick={() => setOpen(true)}
+            icon={<PlusOutlined />}
+          >
+            Create new listing
+          </Button>
           <LogoutBtn />
         </div>
       </Header>
       <Content className="host-content">
         <div className="host-content__wrapper">
-          <Flex gap="middle">
-            {renderHostItems(test_data)}
-          </Flex>
+          <Flex gap="middle">{renderHostItems(test_data)}</Flex>
         </div>
       </Content>
+      <Modal
+        open={open}
+        title="Create new listing"
+        okText="Create"
+        cancelText="Cancel"
+        okButtonProps={{ autoFocus: true, htmlType: "submit" }}
+        onCancel={() => setOpen(false)}
+        destroyOnHidden
+        modalRender={(dom) => (
+          <Form
+            layout="horizontal"
+            form={form}
+            labelCol={{ span: 4 }}
+            wrapperCol={{ span: 18 }}
+            name="form_in_modal"
+            labelAlign="right"
+            clearOnDestroy
+            onFinish={(values) => onCreate(values)}
+          >
+            {dom}
+          </Form>
+        )}
+      >
+        <Form.Item
+          name="title"
+          label="Title"
+          rules={[
+            {
+              required: true,
+              message: "Please input the title of collection!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="description" label="Description">
+          <Input type="textarea" />
+        </Form.Item>
+      </Modal>
     </Layout>
   );
 };
