7ed5e054c1773d5abc1b675bacd4889b16dcd95e
Alex Wang <1262171417@qq.com>
2025-11-07 01:02:03 +1100

    feat: complete create listing modal, except bedrooms

--- frontend/src/pages/Listing/Host.jsx
+++ frontend/src/pages/Listing/Host.jsx
@@ -1,6 +1,21 @@
 import { useState } from "react";
-import { Layout, Typography, Button, Flex, Form, Input, Modal } from "antd";
-import { UnorderedListOutlined, PlusOutlined } from "@ant-design/icons";
+import {
+  Layout,
+  Typography,
+  Button,
+  Flex,
+  Form,
+  Input,
+  Modal,
+  Radio,
+  InputNumber,
+  Upload,
+} from "antd";
+import {
+  UnorderedListOutlined,
+  PlusOutlined,
+  InboxOutlined,
+} from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import LogoutBtn from "../../components/LogoutBtn";
 import HostItem from "../../components/HostItem";
@@ -10,10 +25,51 @@ import "./Host.scss";
 const { Header, Content } = Layout;
 const { Title } = Typography;
 
+const formRules = {
+  title: [
+    {
+      required: true,
+      message: "Please input the title of host listing!",
+    },
+  ],
+  address: [
+    {
+      required: true,
+      message: "Please input the address of host listing!",
+    },
+  ],
+  price: [
+    {
+      required: true,
+      message: "Please input the price per night of host listing!",
+    },
+  ],
+  property_type: [
+    {
+      required: true,
+      message: "Please select the property type of host listing!",
+    },
+  ],
+  bathrooms: [
+    {
+      required: true,
+      message: "Please input the number of bathrooms of host listing!",
+    },
+  ],
+};
+
 const renderHostItems = (listings) => {
   return listings.map((listing) => (
     <HostItem key={listing.id} listing={listing} />
   ));
+};
+
+const normFile = (e) => {
+  console.log("Upload event:", e);
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
 };
 
 const Host = () => {
@@ -70,12 +126,14 @@ const Host = () => {
         okButtonProps={{ autoFocus: true, htmlType: "submit" }}
         onCancel={() => setOpen(false)}
         destroyOnHidden
+        width="90%"
+        style={{ maxWidth: "800px" }}
         modalRender={(dom) => (
           <Form
             layout="horizontal"
             form={form}
-            labelCol={{ span: 4 }}
-            wrapperCol={{ span: 18 }}
+            labelCol={{ span: 6 }}
+            wrapperCol={{ span: 14 }}
             name="form_in_modal"
             labelAlign="right"
             clearOnDestroy
@@ -85,20 +143,57 @@ const Host = () => {
           </Form>
         )}
       >
-        <Form.Item
-          name="title"
-          label="Title"
-          rules={[
-            {
-              required: true,
-              message: "Please input the title of collection!",
-            },
-          ]}
-        >
+        <Form.Item name="title" label="Title" rules={formRules.title}>
           <Input />
         </Form.Item>
-        <Form.Item name="description" label="Description">
-          <Input type="textarea" />
+        <Form.Item name="address" label="Address" rules={formRules.address}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="price" label="Price" rules={formRules.price}>
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="property_type"
+          label="Property Type"
+          rules={formRules.property_type}
+        >
+          <Radio.Group>
+            <Radio value="Apartment">Apartment</Radio>
+            <Radio value="House">House</Radio>
+            <Radio value="Townhouse">Townhouse</Radio>
+            <Radio value="Land">Land</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item
+          name="bathrooms"
+          label="Bathrooms"
+          rules={formRules.bathrooms}
+        >
+          <InputNumber min={1} />
+        </Form.Item>
+
+        <Form.Item name="amenities" label="Property amenities">
+          <Input.TextArea rows={4} />
+        </Form.Item>
+        <Form.Item label="Thumbnail">
+          <Form.Item
+            name="thumbnail "
+            valuePropName="fileList"
+            getValueFromEvent={normFile}
+            noStyle
+          >
+            <Upload.Dragger name="files" action="/upload.do">
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag file to this area to upload
+              </p>
+              <p className="ant-upload-hint">
+                Support for a single or bulk upload.
+              </p>
+            </Upload.Dragger>
+          </Form.Item>
         </Form.Item>
       </Modal>
     </Layout>
